72fcbbf18056a294fc1da7d9da133f60a5790bda
DevSBA <mansur.tallyn@gmail.com>
2025-04-10 17:12:03 -0400

    get

---
+++ src/main/java/net/sba/doors/DoorsEntitiesClient.java
@@ -0,0 +1,10 @@
+package net.sba.doors;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DoorsEntitiesClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/sba/doors/DoorsEntitiesDataGenerator.java
@@ -0,0 +1,11 @@
+package net.sba.doors;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DoorsEntitiesDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
